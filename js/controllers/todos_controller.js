1fc71d2cbe48ddb0737683d71fe8441cfebfe327
Luu Gia Thuy <luugiathuy@gmail.com>
2014-07-15 23:19:58 +0800

    indicate all todos are completed

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -33,6 +33,10 @@ Todos.TodosController = Ember.ArrayController.extend({
         return this.filterBy('isCompleted', true).get('length');
     }.property('@each.isCompleted'),
 
+    allAreDone: function(key, value) {
+        return !!this.get('length') && this.isEvery('isCompleted');
+    }.property('@each.isCompleted'),
+
     remaining: function() {
         return this.filterBy('isCompleted', false).get('length');
     }.property('@each.isCompleted'),
